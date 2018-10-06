9a93d34d7dbb5a78bd8144c81f4abe659dc65fa1
Nicolas <nikba1234321@gmail.com>
2018-10-06 19:13:01 -0400

    SonarCloud Integration

---
+++ src/main/java/com/mcgill/ca/tutorial3/App.java
@@ -0,0 +1,12 @@
+package com.mcgill.ca.tutorial3;
+
+public class App
+{
+    private int x;
+
+    public static void main( String[] args )
+    {
+    	String s = null;
+    }
+
+}
---
+++ src/test/java/com/mcgill/ca/tutorial3/AppTest.java
@@ -0,0 +1,11 @@
+package com.mcgill.ca.tutorial3;
+
+import org.junit.Test;
+
+public class AppTest
+{
+	@Test
+	public void mainTest() {
+  	  App.main(null);
+	}
+}
