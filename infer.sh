2d86cc818ab44563f92872cb17250af790ee4c0c
Nicolas <nikba1234321@gmail.com>
2018-10-07 20:14:02 -0400

    infer test 5

--- infer.sh
+++ infer.sh
@@ -1,7 +1,6 @@
-# Checkout Infer
-git clone https://github.com/facebook/infer.git
-cd infer
-# Compile Infer
-./build-infer.sh java
-# install Infer system-wide...
-sudo make install
+VERSION=0.15.0; \
+curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz" \
+| sudo tar -C /opt -xJ && \
+ln -s "/opt/infer-linux64-v$VERSION/bin/infer" /usr/local/bin/infer
+
+infer run -- ./gradlew build
